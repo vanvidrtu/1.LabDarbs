4da610c1ce925a2016e552d132b0d17afa1da988
vanesa_vidina_241RDBR152 <vanesa.vidina@edu.rtu.lv>
2025-02-12 11:56:55 +0000

    1.uzd

--- LL_1.py
+++ LL_1.py
@@ -23,19 +23,23 @@ class LinkedList:
         
 
     # WRITE FIND_MIDDLE_NODE METHOD HERE #
-    #                                    #
-    #                                    #
-    #                                    #
-    #                                    #
-    ######################################
+    def find_middle_node(self):
+        slow = self.head
+        fast = self.head
 
 
+        while (fast is not None and fast.next is not None):
+            fast = fast.next.next
+            slow = slow.next
 
+        return slow
+    
 my_linked_list = LinkedList(1)
 my_linked_list.append(2)
 my_linked_list.append(3)
 my_linked_list.append(4)
 my_linked_list.append(5)
+# my_linked_list.append(6)
 
 print( my_linked_list.find_middle_node().value )
 
--- LL_2.py
+++ LL_2.py
@@ -22,11 +22,19 @@ class LinkedList:
         return True
 
     # WRITE HAS_LOOP METHOD HERE #
-    #                            #
-    #                            #
-    #                            #
-    #                            #
-    ##############################
+    def has_loop(self):
+        fast = self.head
+        slow = self.head
+
+        while (fast is not None and fast.next is not None):
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        
+        return False
+
+
     
     
     
@@ -34,6 +42,7 @@ class LinkedList:
 my_linked_list_1 = LinkedList(1)
 my_linked_list_1.append(2)
 my_linked_list_1.append(3)
+my_linked_list_1.append(4)
 my_linked_list_1.append(4)
 my_linked_list_1.tail.next = my_linked_list_1.head
 print(my_linked_list_1.has_loop() ) # Returns True
@@ -45,6 +54,7 @@ my_linked_list_2 = LinkedList(1)
 my_linked_list_2.append(2)
 my_linked_list_2.append(3)
 my_linked_list_2.append(4)
+my_linked_list_2.append(3)
 print(my_linked_list_2.has_loop() ) # Returns False
 
 
